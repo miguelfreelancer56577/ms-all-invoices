96fe099c6e2908224e9724cef6aca7549219a000
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2019-09-02 16:51:28 -0500

    first commit

---
+++ src/main/java/com/github/mangelt/ms_all_invoices/App.java
@@ -0,0 +1,13 @@
+package com.github.mangelt.ms_all_invoices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World!" );
+	}
+}
